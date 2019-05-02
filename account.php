e67865141bfb6b5913a35932463cc2f344cb9274
Clay Emmel <clayemmel@gmail.com>
2019-05-02 16:31:23 -0400

    search functionality

--- account.php
+++ account.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html>
 <head>
 
 <!-- Latest compiled and minified CSS -->
@@ -7,40 +5,46 @@
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
 
 <?php
+$username = "bcalabre";
+$password = "nqjvrUHZ";
+$database = "bcalabre_1";
+$mysqli = new mysqli("localhost", $username, $password, $database);
 $email = $_POST['email'];
-search($email);
+$query = "SELECT * FROM Account where email='$email'";
 
-function search($email){
-    require_once('db_setup.php');
-    $sql = "USE bcalabre_1;";
-    if ($conn->query($sql) === TRUE) {
-    } else {
-    echo "Error using  database: " . $conn->error;
-    }
+$fname="";
+$lname="";
+$userpassword="";
 
-    $sql = "SELECT * FROM Account WHERE email=$email";
+$conn = mysql_connect($bclabre_1, $username, $password);
 
-    $result = $conn->query($sql);
+   if(! $conn ) {
+      die('Could not connect: ' . mysql_error());
+   }
 
-    if ($result === TRUE) {
-        return $result;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    header("Location: account.html?email=" + $email);
-}
+   //$sql = 'SELECT * FROM Account where email='$email'';
+   mysql_select_db('bcalabre_1');
+   $retval = mysql_query( $query, $conn );
+
+   if(! $retval ) {
+      die('Could not get data: ' . mysql_error());
+   }
+   while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+      $email = $row['Email'];
+      $fname = $row['Firstname'];
+      $lname = $row['Lastname'];
+      $userpassword = $row['Password'];
+   }
+
+  //echo "value " . $email . "<br>";
+//$conn->close();
+header("Location: account.html?email=$email&fname=$fname&lname=$lname&password=$userpassword");
 ?>
-
-<?php
-$conn->close();
-?>
-
 </body>
 </html>
