3f0e23a1924790d15d1bea7150aa3052020082b7
Clay Emmel <clayemmel@gmail.com>
2019-05-02 21:19:08 -0400

    fixes

--- character_create.php
+++ character_create.php
@@ -17,7 +17,6 @@
 require_once('db_setup.php');
 $sql = "USE bcalabre_1;";
 if ($conn->query($sql) === TRUE) {
-   // echo "using Database tbiswas2_company";
 } else {
    echo "Error using  database: " . $conn->error;
 }
@@ -31,7 +30,6 @@ $sql = "INSERT INTO Avatar values ('$ID','$email', '$displayname', '$class', '0'
 
 
 
-#$sql = "SELECT * FROM Students where Username like 'amai2';";
 $result = $conn->query($sql);
 
 if ($result === TRUE) {
@@ -39,10 +37,7 @@ if ($result === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-//$stmt = $conn->prepare("Select * from Students Where Username like ?");
-//$stmt->bind_param("s", $username);
-//$result = $stmt->execute();
-//$result = $conn->query($sql);
+
 ?>
 
 <?php
--- clan_search.php
+++ clan_search.php
@@ -37,9 +37,9 @@ $conn = mysql_connect($bclabre_1, $username, $password);
       die('Could not get data: ' . mysql_error());
    }
    while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
-      $leaderid = $row['leaderid'];
-      $minlevel = $row['minlevel'];
-      $name = $row['name'];
+      $leaderid = $row['ClanLeaderID'];
+      $minlevel = $row['MinLevel'];
+      $name = $row['Name'];
    }
 
 header("Location: clan.html?name=$name&leaderid=$leaderid&minlevel=$minlevel&operation=$operation&status=$status#searchForm");
